406fbd7da6ff564fef83e6437a2bc770844cfd67
Jean Tituaña <jeantituana@hotmail.com>
2023-11-26 18:14:29 -0500

    Componentes

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab3',
@@ -7,6 +8,17 @@ import { Component } from '@angular/core';
 })
 export class Tab3Page {
 
-  constructor() {}
+  constructor(private alertController: AlertController) {}
+
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'Aviso',
+      subHeader: 'Estas seguro de salir?',
+      message: 'Las fotos que no se hayan guardado, se borraran',
+      buttons: ['Continuar','Salir'],
+    });
+
+    await alert.present();
+  }
 
 }
